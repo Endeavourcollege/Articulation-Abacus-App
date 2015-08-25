e3bb5c7b07af98fda2c19eb8cf79892c2af475d1
Matthew Aisthorpe <mattyace4@hotmail.com>
2015-08-25 15:53:18 +1000

    Filters Correctly
    Added new filter to controllers js to reverse the order of the abacus
    history items.
    
    Modified menu navigation removing old pages.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,6 +5,26 @@ angular.module('starter.controllers', [])
   return $firebaseAuth(usersRef);
 })
 
+.filter('reverse', function() {
+  function toArray(list) {
+     var k, out = [];
+     if( list ) {
+        if( angular.isArray(list) ) {
+           out = list;
+        }
+        else if( typeof(list) === 'object' ) {
+           for (k in list) {
+              if (list.hasOwnProperty(k)) { out.push(list[k]); }
+           }
+        }
+     }
+     return out;
+  }
+  return function(items) {
+     return toArray(items).slice().reverse();
+  };
+})
+   
 .controller('AppCtrl', function($scope, $ionicModal, $timeout, $location, Auth) {
   console.log('AppCtrl');
   
